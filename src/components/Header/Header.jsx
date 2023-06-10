1cb41924b58bb7f96c55b247a1c425e0bf888887
xCoder <xcoder@xCoders-iMac.local>
2023-06-10 09:56:53 +0600

    Header CSS create

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/clipping-path'>Clipping Path</Link>
-            <Link to='/contact'>Contact</Link>
-
-        </div>
+        <>
+            <div >
+                <Link className='space' to='/'>Home</Link>
+                <Link className='space' to='/clipping-path'>Clipping Path</Link>
+                <Link className='space' to='/contact'>Contact</Link>
+                <Link className='space' to='/login'>Login</Link>
+            </div>
+        </>
     );
 };
 
