667596822033823a50f2b86d86e33491705d6529
xCoder <xcoder@xCoders-iMac.local>
2023-06-14 18:33:05 +0600

    U

--- src/Auth/Signup/Signup.jsx
+++ src/Auth/Signup/Signup.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
+
+    const [email, setEmail] = useState('');
+
+    const handleEmailField = (e) => {
+        console.log(e.target.value);
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordField = (e) => {
+    console.log
+}
+
     return (
         <>
             <h3>This Register Form</h3>
             <br />
             <form>
-                <input type="email" name="email" id="email" />
+                <input onChange={handleEmailField} type="email" name="email" id="email" placeholder='Provide your email' />
+                <br /> <br />
+                <input onBlur={handlePasswordField} type="password" name="password" id="password" placeholder='Provide your password' /><br />
                 <br />
-                <input type="password" name="password" id="password" /><br />
                 <button type="submit">Sign Up</button>
             </form>
         </>
