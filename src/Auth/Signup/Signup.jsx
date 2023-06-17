508f97b017eaf4cfb52df76ff457e6bad3859d67
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-06-17 08:26:24 +0600

    code update

--- src/Auth/Signup/Signup.jsx
+++ src/Auth/Signup/Signup.jsx
@@ -11,17 +11,51 @@ const Signup = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setSuccess('');
+        setError('');
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
+
+        // validate email
+        if (!/(?=.*[A-Z])/.test(password)) {
+            setError('Use Upper case');
+            return;
+        }
+        else if (!/(?=.*[a-z])/.test(password)) {
+            setError('Use a lower case')
+            return;
+            
+        }
+        else if (!/(?=.*[a-z])/.test(password)) {
+            setError('Use a lower case')
+            return;
+        }
+        else if (!/(?=.*[0-9])/.test(password)) {
+            setError('Use a number case')
+            return;
+        }
+        else if (!/(?=.*[#?!@$%^&*-])/.test(password)) {
+            setError('Use a special case')
+            return;
+        }
+        else if (password.length < 6) {
+            setError('Provide your password at least 6 char');
+            return;
+        }
+
         console.log(email, password);
         createUserWithEmailAndPassword(auth, email, password)
             .then(userCredentail => {
                 const loggedUser = userCredentail.user;
                 console.log(loggedUser);
+                setError('');
+                e.target.reset();
+                setSuccess('User has been creaate successfully')
             })
             .catch(error => {
                 console.error(error.message);
+                setError(error.message);
             })
     }
 
@@ -47,6 +81,9 @@ const Signup = () => {
                     <input className='w-50 mb-4' onBlur={handlePasswordField} type="password" name="password" id="password" placeholder='Provide your password' required />
                     <button className='w-25' type="submit">Sign Up</button>
                 </form>
+
+                <p className='text-danger'><small>{error}</small></p>
+                <p className='text-success'><small>{success}</small></p>
             </div>
         </>
     );
